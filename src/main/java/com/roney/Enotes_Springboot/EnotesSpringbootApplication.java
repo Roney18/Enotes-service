f6abfa092b26175bade32575a289be3c887f0b6d
roney <roneydhara@gmail.com>
2025-07-23 06:08:38 +0530

    initial commit

---
+++ src/main/java/com/roney/Enotes_Springboot/EnotesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.roney.Enotes_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roney/Enotes_Springboot/EnotesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roney.Enotes_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
